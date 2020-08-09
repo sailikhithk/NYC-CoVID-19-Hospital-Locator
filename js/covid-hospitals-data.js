19d3f4fbf3c520a1b0a49994d82c5978703ddec6
SaiLikhith Kanuparthi <slk522@nyu.edu>
2020-08-09 15:50:58 -0400

    This is done finally

---
+++ js/covid-hospitals-data.js
@@ -0,0 +1,202 @@
+let hospitals = [{
+    "name": "Rockefeller University Hospital",
+    "phoneNumber": "(212) 570-8000",
+    "openTime": "Open 24 Hours",
+    "coordinates": {
+        "latitude": 40.763049,
+        "longitude": -73.956535,
+    },
+
+    "address": "1230 YORK AVENUE",
+    "addressLines": [
+        "Rockefeller University Hospital", "1230 York Avenue, New York, New York 10021"],
+    "pincode": "10021",
+    "zipCode": "10021", /* first few digits */
+    "numBeds": "513",
+    "rating": "3.6",
+},
+
+{
+    "name": "Hospital For Special Surgery",
+    "phoneNumber": "(212) 606-1000",
+    "openTime": "Open 24 Hours",
+    "coordinates": {
+        "latitude": 40.764866,
+        "longitude": -73.953128,
+    },
+    "address": "535 EAST 70TH STREET",
+    "addressLines": ["Hospital For Special Surgery", "535 East 70th Street, New York, New York 10021"],
+    "pincode": "10021",
+    "zipCode": "10021", /* first few digits */
+    "numBeds": "215",
+    "rating": "3.4",
+},
+
+{
+    "name": "Lenox Hill Hospital",
+    "phoneNumber": "(212) 439-2345",
+    "openTime": "Open 24 Hours",
+    "coordinates": {
+        "latitude": 40.773964,
+        "longitude": -73.960611,
+    },
+    "address": "100 EAST 77TH STREET",
+    "addressLines": ["Lenox Hill Hospital", "100 East 77Th Street, New York, New York 10021"],
+    "pincode": "10021",
+    "zipCode": "10021", /* first few digits */
+    "numBeds": "348",
+    "rating": "3.7",
+},
+
+
+{
+    "name": "N Y Eye And Ear Infirmary",
+    "phoneNumber": "(212) 979-4000",
+    "openTime": "Open 24 Hours",
+    "coordinates": {
+        "latitude": 40.732559,
+        "longitude": -73.984816,
+    },
+    "address": "230 SECOND AVE",
+    "addressLines": ["N Y Eye And Ear Infirmary", "230 Second Ave, New York, New York 10003"],
+    "pincode": "10003",
+    "zipCode": "10003", /* first few digits */
+    "numBeds": "16",
+    "rating": "3.1",
+},
+
+{
+    "name": "Mount Sinai West",
+    "phoneNumber": "(212) 523-4000",
+    "openTime": "Open 24 Hours",
+    "coordinates": {
+        "latitude": 40.770129,
+        "longitude": -73.987846,
+    },
+    "address": "1000 TENTH AVENUE",
+    "addressLines": ["Mount Sinai West", "1000 Tenth Avenue, New York, New York 10019"],
+    "pincode": "10019",
+    "zipCode": "10019", /* first few digits */
+    "numBeds": "515",
+    "rating": "3",
+},
+
+
+{
+    "name": "Bellevue Hospital Center",
+    "phoneNumber": "(212) 562-4141",
+    "openTime": "Open 24 Hours",
+    "coordinates": {
+        "latitude": 40.740079,
+        "longitude": -73.97625,
+    },
+    "address": "462 FIRST AVENUE",
+    "addressLines": ["Bellevue Hospital Center", "462 First Avenue, New York, New York 10016"],
+    "pincode": "10016",
+    "zipCode": "10016", /* first few digits */
+    "numBeds": "150",
+    "rating": "4.1",
+},
+
+
+{
+    "name": "Harlem Hospital Center",
+    "phoneNumber": "(212) 939-1000",
+    "openTime": "Open 24 Hours",
+    "coordinates": {
+        "latitude": 40.814482,
+        "longitude": -73.940495,
+    },
+    "address": "506 LENOX AVENUE",
+    "addressLines": ["Harlem Hospital Center", "506 Lenox Avenue, New York, New York 10037"],
+    "pincode": "10037",
+    "zipCode": "10037", /* first few digits */
+    "numBeds": "190",
+    "rating": "4.4",
+},
+
+
+{
+    "name": "Mount Sinai Hospital",
+    "phoneNumber": "(212) 241-7981",
+    "openTime": "Open 24 Hours",
+    "coordinates": {
+        "latitude": 40.7897618,
+        "longitude": -73.9533073,
+    },
+    "address": "ONE GUSTAVE L LEVY PLACE",
+    "addressLines": ["Mount Sinai Hospital", "One Gustave L Levy Place, New York, New York 10029"],
+    "pincode": "10029",
+    "zipCode": "10029", /* first few digits */
+    "numBeds": "943",
+    "rating": "4.4",
+},
+
+
+{
+    "name": "Nyu Langone Hospitals",
+    "phoneNumber": "(212) 263-7300",
+    "openTime": "Open 24 Hours",
+    "coordinates": {
+        "latitude": 40.74226,
+        "longitude": -73.974661,
+    },
+    "address": "550 FIRST AVENUE",
+    "addressLines": ["Nyu Langone Hospitals", "550 First Avenue, New York, New York 10016"],
+    "pincode": "10016",
+    "zipCode": "10016", /* first few digits */
+    "numBeds": "1166",
+    "rating": "3.5",
+},
+
+
+{
+    "name": "Mount Sinai Beth Israel",
+    "phoneNumber": "(212) 420-2000",
+    "openTime": "Open 24 Hours",
+    "coordinates": {
+        "latitude": 40.732665,
+        "longitude": -73.9816,
+    },
+    "address": "FIRST AVENUE AT 16TH STREET",
+    "addressLines": ["Mount Sinai Beth Israel", "First Avenue At 16th Street, New York, New York 10003"],
+    "pincode": "10003",
+    "zipCode": "10003", /* first few digits */
+    "numBeds": "449",
+    "rating": "3.8",
+},
+
+{
+    "name": "Metropolitan Hospital Center",
+    "phoneNumber": "(212) 423-6262",
+    "openTime": "9am-4pm",
+    "coordinates": {
+        "latitude": 40.78472,
+        "longitude": -73.943636,
+    },
+    "address": "1901 FIRST AVENUE",
+    "addressLines": ["Metropolitan Hospital Center", "1901 First Avenue, New York, New York 10029"],
+    "pincode": "10029",
+    "zipCode": "10029", /* first few digits */
+    "numBeds": "158",
+    "rating": "2.9",
+},
+
+{
+    "name": "New York-Presbyterian Hospital-New York Weill Corn",
+    "phoneNumber": "(212) 746-5454",
+    "openTime": "Open 24 hours",
+    "coordinates": {
+        "latitude": 40.76415,
+        "longitude": -73.954335,
+    },
+    "address": "525 EAST 68TH STREET",
+    "addressLines": ["New York-Presbyterian Hospital-New York Weill Corn", "525, East 68th Street, New York, New York 10065"],
+    "pincode": "10065",
+    "zipCode": "10065", /* first few digits */
+    "numBeds": "1792",
+    "rating": "2.8",
+},
+]
+
+
---
+++ js/index.js
@@ -0,0 +1,316 @@
+
+ var map;
+ var markers = [];
+ var infoWindow;
+ var currLatitude;
+ var currLongitude;
+
+ function initMap() {
+    var New_York = {
+        lat: 40.7128,
+        lng: -74.0060,
+    }
+     var images = {
+         url: 'https://cdn.icon-icons.com/icons2/1465/PNG/512/588hospital_100778.png',
+         // This marker is 20 pixels wide by 32 pixels high.
+         size: new google.maps.Size(20, 32),
+         // The origin for this image is (0, 0).
+         origin: new google.maps.Point(0, 0),
+         // The anchor for this image is the base of the flagpole at (0, 32).
+         anchor: new google.maps.Point(0, 32)
+     };
+     map = new google.maps.Map(document.getElementById('map'), {
+     center: New_York,
+     zoom: 15,
+     styles: [
+        {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
+        {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
+        {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
+        {
+          featureType: 'administrative.locality',
+          elementType: 'labels.text.fill',
+          stylers: [{color: '#d59563'}]
+        },
+        {
+          featureType: 'poi',
+          elementType: 'labels.text.fill',
+          stylers: [{color: '#d59563'}]
+        },
+        {
+          featureType: 'poi.park',
+          elementType: 'geometry',
+          stylers: [{color: '#263c3f'}]
+        },
+        {
+          featureType: 'poi.park',
+          elementType: 'labels.text.fill',
+          stylers: [{color: '#6b9a76'}]
+        },
+        {
+          featureType: 'road',
+          elementType: 'geometry',
+          stylers: [{color: '#38414e'}]
+        },
+        {
+          featureType: 'road',
+          elementType: 'geometry.stroke',
+          stylers: [{color: '#212a37'}]
+        },
+        {
+          featureType: 'road',
+          elementType: 'labels.text.fill',
+          stylers: [{color: '#9ca5b3'}]
+        },
+        {
+          featureType: 'road.highway',
+          elementType: 'geometry',
+          stylers: [{color: '#746855'}]
+        },
+        {
+          featureType: 'road.highway',
+          elementType: 'geometry.stroke',
+          stylers: [{color: '#1f2835'}]
+        },
+        {
+          featureType: 'road.highway',
+          elementType: 'labels.text.fill',
+          stylers: [{color: '#f3d19c'}]
+        },
+        {
+          featureType: 'transit',
+          elementType: 'geometry',
+          stylers: [{color: '#2f3948'}]
+        },
+        {
+          featureType: 'transit.station',
+          elementType: 'labels.text.fill',
+          stylers: [{color: '#d59563'}]
+        },
+        {
+          featureType: 'water',
+          elementType: 'geometry',
+          stylers: [{color: '#17263c'}]
+        },
+        {
+          featureType: 'water',
+          elementType: 'labels.text.fill',
+          stylers: [{color: '#515c6d'}]
+        },
+        {
+          featureType: 'water',
+          elementType: 'labels.text.stroke',
+          stylers: [{color: '#17263c'}]
+        }
+     ]
+     });
+
+     /* Initialise the Info Window */
+     infoWindow = new google.maps.InfoWindow();
+
+     /* Initialize the Hospital list */
+     searchHospitals();
+     setOnClickListener();
+     getLocation();
+ }
+
+ function displayhospitals (hospitals) {
+     var hospitalsList ="";
+     var address;
+     var phone;
+    hospitals.forEach(function(hospital, index) {
+        address = hospital.addressLines;
+        phone = hospital.phoneNumber;
+        numBed = hospitals.numBeds;
+        hospitalsList += `
+        <div class="hospital-container">
+            <div class="hospital-container-background">
+                <div class="hospital-info-container"> 
+                    <div class="hospital-address">
+                        <span>${address[0]}</span> 
+                        <span>${address[1]}</span>
+                    </div>
+                    <div class="hospital-phone-number">Ph: ${phone}</div>
+                </div>
+                <div class="hospital-number-container">
+                    <div class="hospital-number">${index+1}</div>
+                </div>
+            </div>
+        </div>
+        `
+    });
+    document.querySelector('.hospitals-list').innerHTML = hospitalsList;
+   }
+
+   /* Show hospitals pin markers on map */
+   function showhospitalsMarker(hospitals, search) {
+    var latlng;
+    var name;
+    var address;
+    var statusText;
+    var phoneNumber;
+    var numBed;
+    var bounds = new google.maps.LatLngBounds();
+    hospitals.forEach(function(hospital, index) {
+    latlng = new google.maps.LatLng(
+        hospital.coordinates.latitude,
+        hospital.coordinates.longitude);
+    name = hospital.name;
+    address = hospital.addressLines[0];
+    statusText = hospital.openTime;
+    phoneNumber = hospital.phoneNumber;
+    numBed = hospital.numBeds;
+    var rating = hospital.rating;
+    
+    createMarker(latlng, name, address, statusText, phoneNumber, numBed, index, rating);
+    setMarkerDirections(hospital.coordinates.latitude, hospital.coordinates.longitude, index);
+    setMarkerAnimations(markers[index], search)
+    bounds.extend(latlng);
+   });
+   map.fitBounds(bounds);
+}
+
+function setMarkerAnimations (marker, search) {
+    if (search == true)
+        marker.setAnimation(google.maps.Animation.BOUNCE);
+    else
+        marker.setAnimation(google.maps.Animation.DROP);
+}
+
+function createMarker(latlng, name, address, statusText, phoneNumber, numBed, index, rating) {
+var html = `
+    <div class="hospital-info-window">
+        <div class="hospital-info-name">
+            ${name} &nbsp ${rating}<span class="fa fa-star checked fa-xs"></span>
+        </div>
+        <div class="hospital-info-status">
+            ${statusText}
+        </div>
+        <div id="hospital-address-id" class="hospital-info-address">
+            <div class="circle">
+                <i class="fas fa-location-arrow"></i>
+            </div>
+            ${address}
+        </div>
+        <div class="hospital-info-phone">
+            <div class="circle">
+                <i class="fas fa-phone-alt"></i>
+            </div>
+            ${phoneNumber}
+        </div>
+        <div class="hospital-info-beds">
+            <div class="circle">
+                <i class="fas fa-procedures"></i>
+            </div>
+            ${numBed}
+        </div>
+    </div>
+    `
+var marker = new google.maps.Marker({
+    map: map,
+    position: latlng,
+    label: `${index+1}`,
+    icon: {
+        url: './img/hospital.png',
+        size: new google.maps.Size(60, 62),
+    },
+    animation: google.maps.Animation.DROP,
+});
+
+google.maps.event.addListener(marker, 'mouseover', function() {
+    infoWindow.setContent(html);
+    infoWindow.open(map, marker);
+});
+markers.push(marker);
+}
+
+
+
+function setMarkerDirections(lat, long, index) {
+google.maps.event.addListener(markers[index], 'click', function() {
+    setOnClickListenerAddress(lat, long);
+});
+}
+
+/* Open the info window marker on hospital selection in hospitals list */
+function setOnClickListener() {
+    var hospitalElements = document.querySelectorAll('.hospital-container');
+    hospitalElements.forEach(function(elem, index) {
+        elem.addEventListener('click', function() {
+            google.maps.event.trigger(markers[index], 'mouseover');
+        })
+    })
+}
+
+function clearLocations(){
+infoWindow.close();
+for (var i = 0; i < markers.length; i++) {
+    markers[i].setMap(null);
+}
+markers.length = 0; /* Find out why */
+}
+
+/* Allow a user to search for the hospitals using zip code */
+function searchHospitals() {
+    var search = false;
+    var zipCode = document.getElementById('zip-code-input').value;
+    if (zipCode) {
+    var foundHospitals = [];
+    hospitals.forEach(function(hospital){
+        var postal = hospital.zipCode.substring(0,5);
+        if (zipCode == hospital.pincode || zipCode == postal) {
+            foundHospitals.push(hospital);
+            /* FIXME zoom-out a little bit when results are few, it gets too zoomed */
+        } else {
+            /* FIXME:Handle this */
+        }
+    });
+    search = true;
+    clearLocations();
+} else {
+    foundHospitals = hospitals; 
+}
+displayhospitals(foundHospitals);
+showhospitalsMarker(foundHospitals, search);
+}
+
+function saveCurrentLocation(position) {
+    currLatitude = position.coords.latitude;
+    currLongitude = position.coords.longitude;
+    console.log(currLatitude, currLongitude);
+ }
+
+function errorHandler(err) {
+    if(err.code == 1) {
+       alert("Welcome");
+    } else if( err.code == 2) {
+      alert("Welcome");
+    }
+ }
+
+function getLocation(){
+    if(navigator.geolocation){
+       // timeout at 60000 milliseconds (60 seconds)
+       var options = {timeout:60000};
+       navigator.geolocation.getCurrentPosition
+       (saveCurrentLocation, errorHandler, options);
+    } else{
+       alert("Sorry, browser does not support geolocation!");
+    }
+ }
+
+function setOnClickListenerAddress(lat, lng) {
+    
+            var url = "https://www.google.com/maps/dir/?api=1";
+            var origin = "&origin=" + currLatitude + "," + currLongitude;
+            var destination = "&destination=" + lat + "," + lng;
+            var newUrl = new URL(url + origin + destination);
+            console.log(newUrl);
+            window.open(newUrl, '_blank');
+
+            
+}
+
+
+
+/* experimental code here */
+
